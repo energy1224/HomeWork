8d7b1ab957b9b42d4030921cc4aa44e1a8d1dd82
Nikolai Iakovenko <energy1224@gmail.com>
2023-10-13 17:51:25 +0300

    Добавил второй вариант решения задачи 2.

--- src/homeLesson6/Task2.java
+++ src/homeLesson6/Task2.java
@@ -17,21 +17,40 @@ public class Task2 {
         char[] char01 = str01.toCharArray();
         char[] char02 = str02.toCharArray();
 
+
+        // вариант 1 . через  StringBuilder
+
+        StringBuilder sb1 = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder();
+
+        sb1.append(str01);
+        sb2.append(str02);
+
+        for (int i = 0; i < sb1.length(); i++) {
+            int index = sb2.indexOf(String.valueOf(sb1.charAt(i)));
+            if (index >= 0) sb2.deleteCharAt(index);
+            else break;
+        }
+        if (sb2.length() == 0) {
+            System.out.println("String " + str01 + " is anаgram of String " + str02);
+        } else {
+            System.out.println("String " + str01 + " is not anаgram of String " + str02);
+        }
+
+       // Вариант 2 . через Arrays.sort();
+
+
         Arrays.sort(char01);
         Arrays.sort(char02);
 
         System.out.println(Arrays.toString(char01));
         System.out.println(Arrays.toString(char02));
 
-       if(Arrays.compare(char01,char02)==0){
-           System.out.println("String "+ str01+ " is anаgram of String "+str02);
-       }
-       else {
-           System.out.println("String "+ str01+ " is not anаgram of String "+str02);
-       }
-
-
-
+        if (Arrays.compare(char01, char02) == 0) {
+            System.out.println("String " + str01 + " is anаgram of String " + str02);
+        } else {
+            System.out.println("String " + str01 + " is not anаgram of String " + str02);
+        }
 
 
     }
